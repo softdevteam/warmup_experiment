badfbb0267a63bcf6ce0f525ac5c3e5d65fab44a
Edd Barrett <vext01@gmail.com>
2015-10-16 15:01:22 +0100

    Add checksum to Java binarytrees.
    Also fix some code-style to fall in line with the rest of the
    benchmark. The original authors used tabs, so be it.

--- benchmarks/binarytrees/java/binarytrees.java
+++ benchmarks/binarytrees/java/binarytrees.java
@@ -1,53 +1,60 @@
 /* The Great Computer Language Shootout
    http://shootout.alioth.debian.org/
- 
+
    contributed by Jarkko Miettinen
 */
 
 public class binarytrees {
     static void init() {};
 
-	private final static int minDepth = 4;
-	
-	public static void main(String[] args){
-		int n = 0;
-		if (args.length > 0) n = Integer.parseInt(args[0]);
-        binarytrees.run(n);
-    }
+	private static final int MIN_DEPTH = 4;
+	private static final int MAX_DEPTH = 12;
+	private static final long EXPECT_CKSUM = -10914;
+	private static long MOD;
+	private static int check = 0;
 
-    public static void run(int n) {
-		
-		int maxDepth = (minDepth + 2 > n) ? minDepth + 2 : n;
+
+	public static void run(int n) {
+		for (int i = 0; i < n; i++) {
+			check = 0;
+			inner_iter(MIN_DEPTH, MAX_DEPTH);
+		}
+	}
+
+	public static void inner_iter(int minDepth, int maxDepth) {
 		int stretchDepth = maxDepth + 1;
-		
-		int check = (TreeNode.bottomUpTree(0,stretchDepth)).itemCheck();
-		//System.out.println("stretch tree of depth "+stretchDepth+"\t check: " + check);
-		
+
+		check += TreeNode.bottomUpTree(0,stretchDepth).itemCheck();
+
 		TreeNode longLivedTree = TreeNode.bottomUpTree(0,maxDepth);
-		
+
 		for (int depth=minDepth; depth<=maxDepth; depth+=2){
 			int iterations = 1 << (maxDepth - depth + minDepth);
-			check = 0;
-			
+
 			for (int i=1; i<=iterations; i++){
 				check += (TreeNode.bottomUpTree(i,depth)).itemCheck();
 				check += (TreeNode.bottomUpTree(-i,depth)).itemCheck();
 			}
-			//System.out.println((iterations*2) + "\t trees of depth " + depth + "\t check: " + check);
-		}	
-		//System.out.println("long lived tree of depth " + maxDepth + "\t check: "+ longLivedTree.itemCheck());
+		}
+
+		check += longLivedTree.itemCheck();
+
+		if (check != EXPECT_CKSUM) {
+			System.out.println("bad check: " + check  + " vs " + EXPECT_CKSUM);
+			System.exit(1);
+		}
 	}
-	
-	
+
+
 	private static class TreeNode
 	{
 		private TreeNode left, right;
 		private int item;
-		
+
 		TreeNode(int item){
 			this.item = item;
 		}
-		
+
 		private static TreeNode bottomUpTree(int item, int depth){
 			if (depth>0){
 				return new TreeNode(
@@ -60,13 +67,13 @@ public class binarytrees {
 				return new TreeNode(item);
 			}
 		}
-		
+
 		TreeNode(TreeNode left, TreeNode right, int item){
 			this.left = left;
 			this.right = right;
 			this.item = item;
 		}
-		
+
 		private int itemCheck(){
 			// if necessary deallocate here
 			if (left==null) return item;
