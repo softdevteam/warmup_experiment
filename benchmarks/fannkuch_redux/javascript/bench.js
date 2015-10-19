3eab1551bd8c0694f94a1e34912acc3b45f04552
Edd Barrett <vext01@gmail.com>
2015-10-19 12:32:38 +0100

    Trailing whitespace in js fannkuch.

--- benchmarks/fannkuch_redux/javascript/bench.js
+++ benchmarks/fannkuch_redux/javascript/bench.js
@@ -1,21 +1,21 @@
 /* The Computer Language Benchmarks Game
    http://benchmarksgame.alioth.debian.org/
 
-   contributed by Isaac Gouy, transliterated from Mike Pall's Lua program 
+   contributed by Isaac Gouy, transliterated from Mike Pall's Lua program
    Modified by Roy Williams.
 */
 
 function run_iter(n) {
   var storage = new ArrayBuffer(12 * n);
-  var p = new Int32Array(storage, 0, n), 
-      q = new Int32Array(storage, n * 4, n), 
+  var p = new Int32Array(storage, 0, n),
+      q = new Int32Array(storage, n * 4, n),
       s = new Int32Array(storage, n * 8, n);
   var sign = 1, maxflips = 0, sum = 0, m = n-1;
-  
-  for(var i=0; i<n; i++){ 
-    p[i] = i; 
-    q[i] = i; 
-    s[i] = i; 
+
+  for(var i=0; i<n; i++){
+    p[i] = i;
+    q[i] = i;
+    s[i] = i;
   }
   do {
     // Copy and flip.
@@ -25,31 +25,31 @@ function run_iter(n) {
         q[i] = p[i];             // Work on a copy.
       }
       var flips = 1;
-      do { 
-        var qq = q[q0]; 
+      do {
+        var qq = q[q0];
         if (qq == 0) {                            // ... until 0th element is 0.
           sum = (sum + sign*flips) | 0;
           if (flips > maxflips) {
             maxflips = flips;   // New maximum?
           }
-          break; 
-        } 
-        q[q0] = q0; 
+          break;
+        }
+        q[q0] = q0;
         if (q0 >= 3) {
-          var i = 1, 
-              j = (q0 - 1) | 0, 
+          var i = 1,
+              j = (q0 - 1) | 0,
               t;
           do {
-            t = q[i]; 
-            q[i] = q[j]; 
-            q[j] = t; 
+            t = q[i];
+            q[i] = q[j];
+            q[j] = t;
             i = (i + 1) | 0;
             j = (j - 1) | 0;
-          } while (i < j); 
+          } while (i < j);
         }
-        q0 = qq; 
-        flips = (flips + 1) | 0; 
-      } while (true); 
+        q0 = qq;
+        flips = (flips + 1) | 0;
+      } while (true);
     }
     // Permute.
     if (sign == 1) {
@@ -57,26 +57,26 @@ function run_iter(n) {
       p[1] = p[0];
       p[0] = t;
       sign = -1; // Rotate 0<-1.
-    } else { 
+    } else {
       var t = p[1];
-      p[1] = p[2]; 
-      p[2] = t; 
+      p[1] = p[2];
+      p[2] = t;
       sign = 1;  // Rotate 0<-1 and 0<-1<-2.
-      for(var i=2; i<n; i++) { 
+      for(var i=2; i<n; i++) {
         var sx = s[i];
         if (sx != 0) {
           s[i] = (sx-1) | 0;
-          break; 
+          break;
         }
-        if (i == m) { 
+        if (i == m) {
           return Array(sum,maxflips);      // Out of permutations.
         }
         s[i] = i;
         // Rotate 0<-...<-i+1.
-        t = p[0]; 
+        t = p[0];
         for(var j=0; j<=i; j++) {
           p[j] = p[j+1];
-        } 
+        }
         p[i+1] = t;
       }
     }
