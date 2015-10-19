4cb5c994ff59ed2eb3c0cc889e6fb3ceaae451ac
Laurence Tratt <laurie@tratt.net>
2015-10-19 17:32:09 +0059

    Get OpenJDK building on OpenBSD.

--- build.sh
+++ build.sh
@@ -21,14 +21,15 @@ check_for svn
 check_for unzip
 check_for xml2-config
 check_for bash
-# java versions need to be jdk7
-# we will build a jdk 8 later.
-# graal build needs both of these!
 check_for java
 check_for javac
+check_for xzdec
 
 case `uname` in
-    OpenBSD) check_for eg++ ;;
+    OpenBSD)
+        check_for egcc
+        check_for eg++
+	;;
 esac
 
 which pypy > /dev/null 2> /dev/null
@@ -140,7 +141,9 @@ build_v8() {
 	# The build actually requires that you clone using this git wrapper tool
 	cd ${wrkdir}
 	OLDPATH=${PATH}
-	PATH=${wrkdir}/depot_tools:${PATH}
+	# v8's build needs python 2.7.10; as we've already built that, we might
+	# as well use it rather than forcing the user to install their own.
+	PATH=${wrkdir}/cpython-inst/bin:${wrkdir}/depot_tools:${PATH}
 	# XXX we should check for errors when fetching, but currently that
 	# causes problems because fetch runs a script which aborts on OpenBSD
 	fetch v8
@@ -167,21 +170,65 @@ build_gmake() {
 	cd make-${GMAKE_V} || exit $?
 	./configure || exit $?
 	make || exit $?
+	cp make gmake
 }
 
-JDK_DIST=openjdk-8-src-b132-03_mar_2014.zip
+JDK_DIST=openjdk-8u45b14-bsd-port-20150618.tar.xz
+JDK_INNER_DIR=openjdk-8u45b14-bsd-port-20150618
+JDK_JAVAC=${wrkdir}/openjdk/build/`uname | tr '[:upper:]' '[:lower:]'`-x86_64-normal-server-release/jdk/bin/javac
 build_jdk() {
 	echo "\n===> Download and build JDK8\n"
-	if [ -f ${wrkdir}/openjdk/build/linux-x86_64-normal-server-release/jdk/bin/javac ]; then return; fi
+	if [ -f ${JDK_JAVAC} ]; then return; fi
 	cd ${wrkdir} || exit $?
-	if ! [ -f "${wrkdir}/openjdk-8-src-b132-03_mar_2014.zip" ]; then
-		wget http://www.java.net/download/openjdk/jdk8/promoted/b132/${JDK_DIST} || exit $?
+	if ! [ -f "${wrkdir}/${JDK_DIST}" ]; then
+		wget http://www.intricatesoftware.com/distfiles/${JDK_DIST} || exit $?
 	fi
-	unzip ${JDK_DIST} || exit $?
+	xzdec ${JDK_DIST} | tar xf - || exit $?
+	mv ${JDK_INNER_DIR} openjdk
 	cd openjdk || exit $?
 	JDK_BUILD_PATH=${wrkdir}/make-${GMAKE_V}:${PATH}
-	PATH=${JDK_BUILD_PATH} bash configure || exit $?
-	PATH=${JDK_BUILD_PATH} make all || exit $?
+	case `uname` in
+	    Linux)
+		PATH=${JDK_BUILD_PATH} bash configure \
+		  --disable-option-checking \
+		  --with-cups-include=/usr/local/include \
+		  --with-jobs=8 \
+                  --with-debug-level=release \
+		  --with-debug-level=release \
+		  --disable-ccache \
+		  --disable-freetype-bundling \
+		  --disable-zip-debug-info \
+		  --disable-debug-symbols \
+		  --enable-static-libjli \
+		  --with-zlib=system \
+		  --with-milestone=fcs \
+		  || exit $?
+	        PATH=${JDK_BUILD_PATH} ../make-${GMAKE_V}/make all || exit $?
+		;;
+	    OpenBSD)
+	        CPPFLAGS=-I/usr/local/include \
+	          LDFLAGS=-L/usr/local/lib \
+	          PATH=${JDK_BUILD_PATH} CC=egcc CXX=eg++ ac_cv_path_NAWK=awk bash configure \
+		  --disable-option-checking \
+		  --with-cups-include=/usr/local/include \
+		  --with-jobs=8 \
+                  --with-debug-level=release \
+		  --with-debug-level=release \
+		  --disable-ccache \
+		  --disable-freetype-bundling \
+		  --disable-zip-debug-info \
+		  --disable-debug-symbols \
+		  --enable-static-libjli \
+		  --with-zlib=system \
+		  --with-giflib=system \
+		  --with-milestone=fcs \
+		  || exit $?
+	        PATH=${JDK_BUILD_PATH} \
+		  COMPILER_WARNINGS_FATAL=false \
+		  DEFAULT_LIBPATH="/usr/lib:/usr/X11R6/lib:/usr/local/lib"\
+		  ../make-${GMAKE_V}/make all || exit $?
+		;;
+	esac
 }
 
 MX_REPO=https://bitbucket.org/allr/mx
@@ -355,8 +402,6 @@ case `uname` in
 	build_v8
 	build_gmake
 	build_jdk
-	build_graal
-	build_jruby_truffle
 	;;
 esac
 fetch_external_benchmarks
