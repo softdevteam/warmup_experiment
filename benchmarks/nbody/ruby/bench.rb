bf239c8eed2b5947825db8a1474c3877b79a05fa
Edd Barrett <vext01@gmail.com>
2015-10-20 16:41:53 +0100

    Epsilon checksum for nbody ruby.

--- benchmarks/nbody/ruby/bench.rb
+++ benchmarks/nbody/ruby/bench.rb
@@ -8,6 +8,10 @@
 SOLAR_MASS = 4 * Math::PI**2
 DAYS_PER_YEAR = 365.24
 
+@EXPECT_CHECKSUM = -0.3381550232201908645635057837353087961673736572265625
+@N_ADVANCES = 100000
+@EPSILON = 0.0000000000001
+
 class Planet
  attr_accessor :x, :y, :z, :vx, :vy, :vz, :mass
 
@@ -78,72 +82,84 @@ def offset_momentum(bodies)
   b.vz = - pz / SOLAR_MASS
 end
 
-BODIES = [
-  # sun
-  Planet.new(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0),
 
-  # jupiter
-  Planet.new(
-    4.84143144246472090e+00,
-    -1.16032004402742839e+00,
-    -1.03622044471123109e-01,
-    1.66007664274403694e-03,
-    7.69901118419740425e-03,
-    -6.90460016972063023e-05,
-    9.54791938424326609e-04),
+def setup_state()
+    bodies = [
+      # sun
+      Planet.new(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0),
 
-  # saturn
-  Planet.new(
-    8.34336671824457987e+00,
-    4.12479856412430479e+00,
-    -4.03523417114321381e-01,
-    -2.76742510726862411e-03,
-    4.99852801234917238e-03,
-    2.30417297573763929e-05,
-    2.85885980666130812e-04),
+      # jupiter
+      Planet.new(
+        4.84143144246472090e+00,
+        -1.16032004402742839e+00,
+        -1.03622044471123109e-01,
+        1.66007664274403694e-03,
+        7.69901118419740425e-03,
+        -6.90460016972063023e-05,
+        9.54791938424326609e-04),
 
-  # uranus
-  Planet.new(
-    1.28943695621391310e+01,
-    -1.51111514016986312e+01,
-    -2.23307578892655734e-01,
-    2.96460137564761618e-03,
-    2.37847173959480950e-03,
-    -2.96589568540237556e-05,
-    4.36624404335156298e-05),
+      # saturn
+      Planet.new(
+        8.34336671824457987e+00,
+        4.12479856412430479e+00,
+        -4.03523417114321381e-01,
+        -2.76742510726862411e-03,
+        4.99852801234917238e-03,
+        2.30417297573763929e-05,
+        2.85885980666130812e-04),
 
-  # neptune
-  Planet.new(
-    1.53796971148509165e+01,
-    -2.59193146099879641e+01,
-    1.79258772950371181e-01,
-    2.68067772490389322e-03,
-    1.62824170038242295e-03,
-    -9.51592254519715870e-05,
-    5.15138902046611451e-05)
-]
+      # uranus
+      Planet.new(
+        1.28943695621391310e+01,
+        -1.51111514016986312e+01,
+        -2.23307578892655734e-01,
+        2.96460137564761618e-03,
+        2.37847173959480950e-03,
+        -2.96589568540237556e-05,
+        4.36624404335156298e-05),
 
+      # neptune
+      Planet.new(
+        1.53796971148509165e+01,
+        -2.59193146099879641e+01,
+        1.79258772950371181e-01,
+        2.68067772490389322e-03,
+        1.62824170038242295e-03,
+        -9.51592254519715870e-05,
+        5.15138902046611451e-05)
+    ]
+end
 
-def run_iter(n)
-    #n = Integer(ARGV[0])
+def inner_iter(n)
+    bodies = setup_state()
+    checksum = 0
 
-    offset_momentum(BODIES)
+    offset_momentum(bodies)
 
-    #puts "%.9f" % energy(BODIES)
-    "%.9f" % energy(BODIES)
+    checksum += energy(bodies)
 
-    nbodies = BODIES.size
+    nbodies = bodies.size
     dt = 0.01
 
     n.times do
       i = 0
       while i < nbodies
-        b = BODIES[i]
-        b.move_from_i(BODIES, nbodies, dt, i + 1)
+        b = bodies[i]
+        b.move_from_i(bodies, nbodies, dt, i + 1)
         i += 1
       end
     end
 
-    #puts "%.9f" % energy(BODIES)
-    "%.9f" % energy(BODIES)
+    checksum += energy(bodies)
+
+    if (checksum - @EXPECT_CHECKSUM).abs >= @EPSILON then
+        puts("bad checksum: %f vs %f" % [checksum, @EXPECT_CHECKSUM])
+        exit(1)
+    end
+end
+
+def run_iter(n)
+  for i in 1..n do
+    inner_iter(@N_ADVANCES)
+  end
 end
