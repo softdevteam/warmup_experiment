078a1f030d46f436b9d64bffed7dcec3f47d985e
Edd Barrett <vext01@gmail.com>
2015-12-18 09:31:21 +0000

    Properly skip Graal build if already done.

--- build.sh
+++ build.sh
@@ -288,7 +288,7 @@ build_graal() {
 		cd ${wrkdir} && hg clone -r ${MX_VERSION} ${MX_REPO} || exit $?
 	fi
 
-	if [ -f ${wrkdir}/jvmci/jdk1.8.0-internal/product/bin/javac ]; then return; fi
+	if [ -f ${wrkdir}/jvmci/jdk1.8*-internal/product/bin/javac ]; then return; fi
 
 	cd ${wrkdir}
 	if ! [ -d ${wrkdir}/graal ]; then
